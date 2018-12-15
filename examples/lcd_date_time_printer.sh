ca9de64e13d6e30818e4d62a3b5c3852aed26c97
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2018-12-15 16:09:02 +0100

    Update lcd_date_time_printer.sh

--- examples/lcd_date_time_printer.sh
+++ examples/lcd_date_time_printer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this bash script prints the current date and time to the LCD display through the LCD named pipe
+
+
 pipePath="/tmp/lcd_pipe" 
 read hh mm day month year jourAbr dayOfWeek< <(date +"%H %M  %d  %m  %Y %a %a");
 textToDisplay="$dayOfWeek.$day/$month/$year $hh:$mm"
