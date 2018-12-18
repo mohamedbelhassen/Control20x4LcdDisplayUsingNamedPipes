aae8db4cf372557cbbc8f88ba261ec57b0797877
MOHAMED BELHASSEN <mohamed.belhassen@gmail.com>
2018-12-18 13:03:05 +0100

    omitting useless code

--- examples/test_lcd.py
+++ examples/test_lcd.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
-
-#this example show how to write text to the LCD by the means of the LCD named pipe from python scripts
-
 import os
+
 #communicate with the LCD controller process through named pipe
-wfPath = "/tmp/lcd_pipe"
-try:
-    os.mkfifo(wfPath)
-except OSError:
-    pass
-wp = open(wfPath, 'w')
+
+#if you have changed the path of the named path of your LCD display, update the following line
+pipePath = "/tmp/lcd_pipe"
+
+#open the pipe file in wrting mode
+wp = open(pipePath, 'w')
 
 #write the expression "Your LCD is well configured" in the LCD display
-wp.write("2|2|your LCD\n")
-wp.write("3|2|is well configured\n")		
+wp.write("2|2|your LCD")
+wp.write("3|2|is well configured")		
+
+#finally, close the file object
 wp.close()
